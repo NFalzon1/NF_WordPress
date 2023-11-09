e17c2f703f9081564ccb3ed8f7db03ee6402f3da
NFalzon1 <nicholas.falzon.d56570@mcast.edu.mt>
2023-11-09 13:14:13 +0100

    Customizer

--- app/public/wp-content/themes/classExample/footer.php
+++ app/public/wp-content/themes/classExample/footer.php
@@ -1,26 +1,47 @@
-<div class="container-fluid footerStyle">
+<?php
+  $footer_bg = get_theme_mod('custom_footer_bg','dark');
+  $footer_class = "bg-".$footer_bg;
+
+  $footer_text = "text-light";
+  if($footer_bg =="light"){
+    $footer_text == "text-dark";
+  }
+
+  $footer_text = get_theme_mod("custom_theme_footer_text","ffffff");
+?>
+
+
+<div class="container-fluid footerStyle <?php echo $footer_class." ".$footer_text ?>">
   <div class="row">
-    <div class="col-2">
-      <nav id="class_example-main-footer">
-        <?php
-          wp_nav_menu( array(
-            'theme_location'=> 'footer-menu',
-          ));
-        ?>
-      </nav>
+   
+  <?php 
+
+  // $footer_layout = get_theme_mod('custom_footer_widget_count','2');
+  // $sidebars_active = false;
+
+  // for($i=0;$i<$footer_layout; $i++){
+  //   if(is_active_sidebar('footer-sidebar-'.($i+1))){
+  //     $sidebars_active = true;
+  //   }
+  // }
+
+  // if($sidebars_active):
+  //   for($i=0; $i<$footer_layout; $i++):
+
+  //     echo "<div class='col'>";
+
+  //     if(is_active_sidebar('footer-sidebar-'.($i+1))){
+  //       dynamic_sidebar('footer-sidebar-'.($i+1));
+  //     };
+
+  //     echo "</div>";
+
+  //   endfor;
+  // endif;
+
+  ?>
     </div>
-    <div class="col-sm">
-      <?php get_sidebar('footer-1'); ?>
-    </div>
-    <div class="col-2">
-      <?php get_sidebar('footer-2'); ?>
-    </div>
-    <div class="col-2">
-      <?php get_sidebar('footer-3'); ?>
-    </div>
-    <div class="col-2">
-      <?php get_sidebar('footer-4'); ?>
-    </div>
+    
     
 </div>
 <!-- Footer -->
--- app/public/wp-content/themes/classExample/functions.php
+++ app/public/wp-content/themes/classExample/functions.php
@@ -3,32 +3,41 @@
 require_once("lib/enqueue-assets.php");
 require_once("lib/navigation.php");
 require_once("lib/sidebars.php");
+require_once("lib/customize.php");
 
 
 show_admin_bar(false);
 
-function classExample_h6title($title){
-    return "<h6>".$title."</h6>";
+function classExample_h6title($title)
+{
+    return "<h6>" . $title . "</h6>";
 }
 
-function classExample_excerptLength($words){
+function classExample_excerptLength($words)
+{
     return 15;
 }
 
-function classExample_themefooter(){
-    echo "<div class='container-fluid bg-warning'><div class='row'><div class='col text-center'><p>Built by &copy; Class Example</p></div></div></div>";
+function classExample_themefooter()
+{
+    $footer_bg = get_theme_mod("custom_theme_footer_bg", "#ffc107");
+    $footer_text = get_theme_mod("custom_theme_footer_text", "#fffff");
+
+
+    echo "<div class='container-fluid' style='background-color:{$footer_bg};'><div class='row'><div class='col text-center'><p style='color:{$footer_text};'>Built by &copy; Class Example</p></div></div></div>";
 }
 
 add_filter('the_title', 'classExample_h6title');
 
-add_filter('excerpt_length','classExample_excerptLength');
+add_filter('excerpt_length', 'classExample_excerptLength');
 
-add_action('get_footer','classExample_themefooter');
+add_action('get_footer', 'classExample_themefooter');
 
-function classexample_postorderasc($query){
-    $query->set('order','ASC');
+function classexample_postorderasc($query)
+{
+    $query->set('order', 'ASC');
 }
 
-add_action('pre_get_posts','classexample_postorderasc');
+add_action('pre_get_posts', 'classexample_postorderasc');
 
 ?>
